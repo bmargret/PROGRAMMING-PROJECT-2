46c757236d2c1155bb7f4cb0ba47d86bbb694f38
Annah-2003 <irenengitau@gmail.com>
2024-10-13 14:57:05 +0300

    part 2

--- main.cpp
+++ main.cpp
@@ -16,4 +16,26 @@ struct Team {
 // Function reads teams from a CSV file saved locally
 vector<Team> readTeamsFromCSV(const string &filename) {
     vector<Team> teams;
+    ifstream file(filename);
+    string line;
+
+    if (!file.is_open()) {
+        cerr << "Error: Could not open the file.\n";
+        return teams;
+    }
+
+    while (getline(file, line)) {
+        stringstream ss(line);
+        string name, town, stadium;
+
+        // Read each field from the line (team name, town, stadium)
+        getline(ss, name, ',');
+        getline(ss, town, ',');
+        getline(ss, stadium, ',');
+
+        teams.push_back({name, town, stadium});
+    }
+
+    return teams;
+}
 
